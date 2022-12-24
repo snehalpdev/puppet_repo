4fc5194df4ded0f1f46b99f6e3765d4a31d4c711
snehalpdev <35354890+snehalpdev@users.noreply.github.com>
2022-12-24 23:47:29 +1100

    Bootstrap Puppet r10k control repo

--- site/profile/manifests/soe.pp
+++ site/profile/manifests/soe.pp
@@ -1,3 +1,96 @@
 class profile::soe {
-  include ::soe 
+  class {'::soe' (
+  # MOTD
+  $motd = true,
+  # Activation ## KMS activation
+  $activate = false,
+  # DNS client
+  $dnsclient = false,
+  # Domain Join
+  $domainjoin = false,
+  # Windows Firewall
+  $firewall = false,
+  # Windows Features
+  $windowsfeatures = false,
+  # Package installer
+  $packageinstall = false,
+  # Splunk Forwarder
+  $splunk_forwarder = false,
+  # Timezone
+  $timezone = false,
+  # Local user management
+  $users = false,
+  # VMtools
+  $vmtools = false,
+  # WinRM
+  $winrm = false,
+  # WSUS client
+  $wsusclient = false,
+  # Crowdstrike Falcon
+  $crowdstrike = false,
+  # CIS Hardening
+  $cis_hardening = false,
+  )
+  {
+
+    if $motd == true {
+      include motd
+    }
+
+  #  if $dnsclient == true {
+  #    dnsclient
+  #  }
+
+  #  if $domainjoin == true {
+  #    include domain_membership
+  #  }
+
+  #  if $windowsfeatures == true {
+  #    include windowsfeatures
+  #  }
+
+  #  if $packageinstall == true {
+  #    include package
+  #  }
+
+  #  if $splunk_forwarder == true {
+  #    include splunk_forwarder
+  #  }
+
+  #  if $timezone == true {
+  #    include timezone
+  #  }
+
+  #  if $users == true {
+  #    include users
+  #  }
+
+  #  if $vmtools == true {
+  #    include vmtools
+  #  }
+
+  #  if $winrm == true {
+  #    include winrm
+  #  }
+
+  #  if $wsusclient == true {
+  #    include wsusclient
+  #  }
+
+  # if $crowdstrike == true {
+  #   include crowdstrike
+  # }
+
+  # if $activate == true {
+  #  include activation
+  # }
+
+  # if $firewall == true {
+  #   include windows_firewall
+  # }
+
+  # if $cis_hardening == true {
+  #   include cis_windows
+  # }
+  }
 }
