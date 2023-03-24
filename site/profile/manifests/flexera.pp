2e5a1e6f228c3eb8456083e42e09dee34a9c1f59
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-24 21:14:37 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -1,6 +1,6 @@
 # Puppet Flexera Class
 class profile::flexera {
-  $bootstrapschedule = 'Bootstrap Machine Schedule'
+  $bootstrapschedule = 'C:\\temp\\fnms\\extract\\Bootstrap Machine Schedule'
   $download_url = hiera('profile::flexera::download_url','http://server02.local/repo/fnms/fnms_agent.zip')
   $install_flexera = hiera('profile::flexera::install_flexera','false')
 
