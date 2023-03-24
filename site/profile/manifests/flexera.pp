1135502442b58cba7442c68b8bebc36eee1d6782
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-24 16:57:16 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -11,8 +11,8 @@ class profile::flexera {
       checksum_type => 'sha256',
       checksum      => '9929bc494433c7843418e4286951df9c055832e6002a8a965402feff85699bac',
       extract       => 'true',
-      creates       => 'C:\\temp\\fnms',
       extract_path  => 'C:\\temp\\fnms',
+      creates       => 'C:\\temp\\fnms',
       cleanup       => 'true',
       before        => Package['FlexNet Inventory Agent'],
     }
