aaf03a5eac3ac08a88a8522a4b5feceee4bdccd8
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-24 15:05:45 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -1,6 +1,6 @@
 # Puppet Flexera Class
 class profile::flexera {
-  $build_dir = "C:\\temp"
+  $build_dir = 'C:/temp/'
   $download_url = hiera('profile::flexera::download_url','http://server02.local/repo/fnms/fnms_agent.zip')
   $install_flexera = hiera('profile::flexera::install_flexera','false')
 
@@ -10,7 +10,8 @@ class profile::flexera {
       checksum_type => 'sha256',
       checksum      => '9929bc494433c7843418e4286951df9c055832e6002a8a965402feff85699bac',
       extract       => 'true',
-      creates       => 'C:\temp\fnms',
+      creates       => 'C:/temp/fnms',
+      extract_path  => 'C:/temp/fnms'
       cleanup       => 'true',
       before        => Package['FlexNet Inventory Agent'],
     }
