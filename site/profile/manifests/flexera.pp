cae9ec15e8049576e1319ce1d1d97a75e66784e2
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-24 14:49:15 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -1,32 +1,31 @@
 # Puppet Flexera Class
 class profile::flexera {
-  $build_dir = 'C:\build'
+  $build_dir = 'C:\temp'
   $download_url = hiera('profile::flexera::download_url','http://server02.local/repo/fnms/fnms_agent.zip')
   $install_flexera = hiera('profile::flexera::install_flexera','false')
 
   if ($install_flexera == 'true') {
-    package { 'wget':
-      ensure => present,
-    }
-
     archive { $build_dir:
-      ensure        => present,
+      ensure        => directory,
       source        => $download_url,
       checksum_type => 'sha256',
       checksum      => '9929bc494433c7843418e4286951df9c055832e6002a8a965402feff85699bac',
+      extract       => 'true',
+      creates       => 'C:\temp\fnms',
+      cleanup       => 'true',
       before        => Package['FlexNet Inventory Agent'],
     }
 
     package { 'FlexNet Inventory Agent':
       ensure          => 'installed',
       provider        => 'windows',
-      source          => "${build_dir}\\FlexNet Inventory Agent.msi",
+      source          => "${build_dir}\\fnms\\FlexNet Inventory Agent.msi",
       install_options => [
         '/qn',
         "BOOTSTRAPSCHEDULE='Bootstrap Machine Schedule'",
         "GENERATEINVENTORY='true'",
         "APPLYPOLICY='true'",
-        "TRANSFORMS=${build_dir}\\InstallFlexNetInvAgent.mst",
+        "TRANSFORMS=${build_dir}\\fnms\\InstallFlexNetInvAgent.mst",
       ],
       before          => Service['ndinit'],
     }
