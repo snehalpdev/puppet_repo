f10fcecd6c190a15abe8039d75f2b08337767116
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-24 16:49:27 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -1,6 +1,6 @@
 # Puppet Flexera Class
 class profile::flexera {
-  $zip_file = 'C:\temp\fnms_agent.zip\n'
+  $zip_file = 'C:\temp\fnms_agent.zip'
   $download_url = hiera('profile::flexera::download_url','http://server02.local/repo/fnms/fnms_agent.zip')
   $install_flexera = hiera('profile::flexera::install_flexera','false')
 
