18c47a474f3617ce838e7c217ae7903478f970c7
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-27 14:25:42 +1100

    updated flexera

--- site/profile/manifests/flexera.pp
+++ site/profile/manifests/flexera.pp
@@ -2,7 +2,7 @@
 class profile::flexera {
   $download_url = hiera('profile::flexera::download_url','http://server02.local/repo/fnms/fnms_agent.zip')
   $install_flexera = hiera('profile::flexera::install_flexera','false')
-  $flexnet_installed = reg_query('HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{F56247D8-9D8D-4180-BD6D-44485EC1A3C0}', { 'type' => 'exists' })
+  $flexnet_installed = registry_value('HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{F56247D8-9D8D-4180-BD6D-44485EC1A3C0}', 'Displayname')
 
   if ($install_flexera == 'true' -AND ($flexnet_installed == 'false')) {
     file { 'C:\temp\fnms':
