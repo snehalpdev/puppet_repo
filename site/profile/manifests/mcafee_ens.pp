0d104a5179ca1f14b367ffd470b8a2cd5dcc191b
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-29 21:29:53 +1100

    updated flexera, crowdstirke and mcafee

--- site/profile/manifests/mcafee_ens.pp
+++ site/profile/manifests/mcafee_ens.pp
@@ -38,7 +38,7 @@ class profile::mcafee_ens (
 
     exec { 'Mcafee ENS':
       cwd         => $extract_dir,
-      command     => 'c:\windows\system32\cmd.exe /C set __COMPAT_LAYER=RUNASINVOKER && start C:\temp\mcafee_ens\extract\setupEP.exe ADDLOCAL="tp" /qn',
+      command     => 'c:\windows\system32\cmd.exe /C "set __COMPAT_LAYER=RUNASINVOKER && start "" C:\temp\mcafee_ens\extract\setupEP.exe ADDLOCAL="tp" /qn"',
       provider    => windows,
       subscribe   => Exec['Extract Mcafee ENS'],
       refreshonly => true,
