9365f865f3abd695ae30c5128021fcb5b0bf0dd0
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-29 13:28:48 +1100

    updated flexera

--- site/profile/manifests/mcafee_ens.pp
+++ site/profile/manifests/mcafee_ens.pp
@@ -38,9 +38,8 @@ class profile::mcafee_ens (
 
     exec { 'Mcafee ENS':
       cwd         => $extract_dir,
-      command     => 'C:\temp\mcafee_ens\extract\setupEP.exe ADDLOCAL="tp" /qn',
+      command     => 'C:\temp\mcafee_ens\extract\setupEP.exe ADDLOCAL="tp"',
       provider    => powershell,
-      user        => 'Administrator',
       subscribe   => Exec['Extract Mcafee ENS'],
       refreshonly => true,
       logoutput   => 'on_failure',
