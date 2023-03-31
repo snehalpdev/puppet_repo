28b7d4d8e744e13e432b5075b9b56483fa3cc80f
Snehal Patel <snehal.p.patel@global.ntt>
2023-03-31 19:27:41 +1100

    updated flexera, crowdstirke and mcafee

--- site/profile/manifests/mcafee_agent.pp
+++ site/profile/manifests/mcafee_agent.pp
@@ -6,7 +6,7 @@
 class profile::mcafee_agent (
   String $build_dir = 'C:\temp',
   String $download_url = hiera('profile::mcafee_agent::download_url','http://server02.local/repo/mcafee_agent/FramePkg.exe'),
-  $install_mcafee = hiera('profile::mcafee_agent::install_mcafee','false')
+  String $install_mcafee = hiera('profile::mcafee_agent::install_mcafee','false')
 ) {
   if ($install_mcafee == 'true') {
     download_file { 'Download Mcafee Agent':
