f22480e08c66fe65a25ba4003f200b9c30a3170d
Snehal Patel <snehal.p.patel@global.ntt>
2023-04-03 12:51:48 +1000

    updated flexera

--- site/profile/manifests/mcafee_ens.pp
+++ site/profile/manifests/mcafee_ens.pp
@@ -43,7 +43,7 @@ class profile::mcafee_ens (
       trigger   => [{
           schedule   => 'once',
           start_date => strftime('%Y-%m-%d'),
-          start_time => strftime('%H:%M:%S', time() + 300),
+          start_time => strftime('%H:%M:%S'),
       }],
       subscribe => Exec['Install Mcafee ENS'],
     }
