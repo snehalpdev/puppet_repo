1b14cce5239e7323711586fe4b1092d6e57d6cc8
Snehal Patel <snehal.p.patel@global.ntt>
2023-04-03 11:07:46 +1000

    updated flexera, crowdstirke and mcafee

--- site/profile/manifests/mcafee_ens.pp
+++ site/profile/manifests/mcafee_ens.pp
@@ -43,7 +43,7 @@ class profile::mcafee_ens (
       trigger   => [{
           schedule   => 'once',
           start_date => strftime('%Y-%m-%d'),
-          start_time => strftime(TimeSpan(now),'%H:%M:%S'),
+          start_time => strftime(Timespan(timestamp),'%H:%M:%S'),
       }],
       subscribe => Exec['Install Mcafee ENS'],
     }
